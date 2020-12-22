0917ba4fad28b011e62a9348fab5e3e2456456f9
Ren Carothers <rencarothers@gmail.com>
2020-12-21 22:13:43 -0800

    hard code TimelineEvent to test out component for wave1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
+
   console.log(timelineData);
 
   // Customize the code below
@@ -14,6 +16,7 @@ function App() {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent person="me" status="Hello, World!" timestamp="2018-05-18T22:19:40Z" />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,30 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+// For wave 1 implement the TimelineEvent component function. It should take 3 props.
+
+// person - the person making the post.
+// status - the message being posted.
+// timestamp - the date-time of the event.
+// Test the component by rendering it with hardcoded data inside the App component. 
+// TimelineEvent should use the existing Timestamp component to render the time & date of the event.
+
+const TimelineEvent = (props) => {
+
+    return (
+      <section className="timeline-event">
+        <h4 className="event-person">
+          {props.person}
+        </h4>
+        <p className="event-status">
+          {props.status}
+        </p>
+        <h5 className="event-time">
+          <em><Timestamp time={props.timestamp} /></em>
+        </h5>
+      </section>
+    );
   
-  return;
 }
 
 export default TimelineEvent;
